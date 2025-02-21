60dab97392adbcc3999b79875ec91ca778f16013
Elen <elen@payment2go.co.id>
2025-02-21 10:42:14 +0700

    Initial Commit

---
+++ personal-blog-cms/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace personal_blog_cms.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
